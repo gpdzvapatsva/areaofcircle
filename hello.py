3d466bc2a6cb387ba48249097668a1786309ab6e
gpdzvapatsva <gpdzvapatsva@gmail.com>
2020-09-16 16:37:00 +0200

    my initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello guys ! How are you?")
+print("Ahwe")
+print(5+9)
+
+firstname=input("What is your name\n")
+print("My name is  ", firstname)
+#===================AREA OF A CIRCLE===========================================
+radius=int(input("please enter radius\n"))
+pi = 3.14
+areaofcircle = pi * radius * radius
+print ( areaofcircle)
+
